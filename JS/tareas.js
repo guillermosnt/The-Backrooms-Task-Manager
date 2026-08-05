624c36293fd88e1853da852d3c2981f81e8877f8
Guillermo Santana García <guille2000sg@gmail.com>
2026-08-05 13:24:56 +0200

    Backrooms Task List

---
+++ JS/tareas.js
@@ -0,0 +1,78 @@
+const listadoTareas = document.getElementById("listado-tareas");
+const input = document.getElementById("inputTarea");
+const boton = document.getElementById("btnAñadir");
+
+function crearTarea(texto) {
+
+    const tarea = document.createElement("div");
+    tarea.classList.add("tarea");
+
+    tarea.innerHTML = `
+    
+    <div class="izquierda">
+
+        <span>${texto}</span>
+
+    </div>
+
+    <div class="centro">
+
+
+
+        <h3>NO COMPLETADO</h3>
+
+    </div>
+
+    <div class="botones-derecha">
+
+        <button class="clip">
+        Ver clip
+        </button>
+
+        <button class="clip papelera">
+        Eliminar
+        </button>
+
+        <button class="clip completado">
+        Marcar como completado
+        </button>
+        
+    </div>
+`;
+
+    const botonCompletar = tarea.querySelector(".completado");
+    const estado = tarea.querySelector("h3");
+
+    botonCompletar.addEventListener("click", function () {
+
+        if (estado.textContent === "NO COMPLETADO") {
+            estado.textContent = "COMPLETADO";
+        } else {
+            estado.textContent = "NO COMPLETADO";
+        }
+
+    });
+
+    const botonEliminar = tarea.querySelector(".papelera");
+    botonEliminar.addEventListener("click", function () {
+        listadoTareas.removeChild(tarea);
+    });
+
+
+    const botonClip = tarea.querySelector(".clip");
+    botonClip.addEventListener("click", function () {
+        alert("¡Clip de la tarea: " + texto + "!");
+    });
+
+    listadoTareas.appendChild(tarea);
+
+}
+
+
+boton.addEventListener("click", function () {
+
+    crearTarea(input.value);
+
+});
+
+
